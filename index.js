11540c9591316517a86f13df35a9e86997b38e78
Gurnav224 <chaudharyg856@gmail.com>
2025-03-20 21:16:45 +0530

    feat: all api added to index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const connectDb = require("./database/dbConnect");
-const userRouter = require('./routes/user.routes');
-const morgan = require('morgan');
+const userRouter = require("./routes/user.routes");
+const teamRouter = require("./routes/team.routes");
+const projectRouter = require("./routes/project.routes");
+const tagRouter = require("./routes/tag.routes");
+const tasksRouter = require("./routes/tasks.routes");
+
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 5000;
 
-connectDb()
+connectDb();
 
 app.get("/", (req, res) => {
-    res.send(` 
+  res.send(` 
 <!DOCTYPE html>
     <html lang="en">
         <head>
@@ -32,8 +37,12 @@ app.get("/", (req, res) => {
     `);
 });
 
-app.use('/auth', userRouter);
+app.use("/auth", userRouter);
+app.use("/team", teamRouter);
+app.use("/project", projectRouter);
+app.use("/tags", tagRouter);
+app.use("/tasks", tasksRouter);
 
 app.listen(PORT, () => {
-    console.log(`server started at http://localhost:${PORT}`);
+  console.log(`server started at http://localhost:${PORT}`);
 });
