ff596fd21c087c0ee5580e9e6ea8bf7bc9d3559f
Gurnav224 <chaudharyg856@gmail.com>
2025-03-28 08:29:27 +0530

    add server origin

--- index.js
+++ index.js
@@ -11,18 +11,12 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors({
-  origin:"*",
-  optionsSuccessStatus:200,
-  credentials:true
-}));
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 204 
+};
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
+app.use(cors(corsOptions))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
