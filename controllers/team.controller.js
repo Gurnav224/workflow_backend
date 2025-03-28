2de58ad6bcb748e923553f9f873f903df4833d5e
Gurnav224 <chaudharyg856@gmail.com>
2025-03-28 06:25:17 +0530

    feat: create api to get team by Id and update

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -1,27 +1,69 @@
 const Team = require("../models/team.model");
+const mongoose = require('mongoose')
 
+exports.createNewTeam = async (req, res) => {
+  const { name, description, members } = req.body;
+  try {
+    const team = new Team({ name, description, members });
+    await team.save();
+    res.status(201).json(team);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "error to create new team" });
+  }
+};
 
-exports.createNewTeam =  async (req, res) => {
-    const {name, description } = req.body;
+exports.getTeams = async (req, res) => {
+  try {
+    const teams = await Team.find({});
+    if (teams.length === 0) {
+      return res.status(404).json({ error: "no team found" });
+    }
+    res.status(200).json(teams);
+  } catch (error) {
+    res.status(500).json({ error: "server error to get teams" });
+  }
+};
+
+exports.getTeamById = async (req, res) => {
+    const {teamId} = req.params;
     try {
-        const team = new Team({name, description});
-        await team.save()
-        res.status(201).json(team);
+        if (!mongoose.Types.ObjectId.isValid(teamId)) {
+            return res.status(400).json({ error: "Invalid team ID" });
+          }
+
+        const team = await Team.findById(teamId);
+        if (!team) {
+            return res.status(404).json({ error: "Team not found" });
+          }
+          res.status(200).json(team)
     } catch (error) {
-        console.log(error)
-        res.status(500).json({error:'error to create new team'})
+        console.error(error);
+    res.status(500).json({ error: "server error" });
+  
     }
 }
 
+exports.updateTeamById = async (req, res) => {
+  const {teamId} = req.params;
+  const { memberName } = req.body;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(teamId)) {
+        return res.status(400).json({ error: "Invalid team ID" });
+      }
 
-exports.getTeams =  async (req , res) => {
-    try {
-        const teams = await Team.find({});
-        if(teams.length === 0) {
-            return res.status(404).json({error:"no team found"})
-        }
-        res.status(200).json(teams)
-    } catch (error) {
-        res.status(500).json({error:"server error to get teams"})
-    }
-}
+
+    const team = await Team.findByIdAndUpdate(teamId, {
+      $push: { members:{memberName} },
+    });
+
+    if (!team) {
+        return res.status(404).json({ error: "Team not found" });
+      }
+
+    res.status(200).json(team);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "server error" });
+  }
+};
--- routes/team.routes.js
+++ routes/team.routes.js
@@ -1,11 +1,12 @@
-const { createNewTeam, getTeams } = require('../controllers/team.controller');
+const { createNewTeam, getTeams, updateTeamById ,getTeamById} = require('../controllers/team.controller');
 
 const router = require('express').Router();
 
 
 router.post('/new', createNewTeam);
 router.get('/all', getTeams);
-
+router.get('/:teamId', getTeamById)
+router.put('/:teamId', updateTeamById)
 
 
 module.exports = router;
