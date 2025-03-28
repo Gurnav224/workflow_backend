b2e72227327528e102178500b6c5df42357454de
Gurnav224 <chaudharyg856@gmail.com>
2025-03-28 06:47:25 +0530

    fix: cors origin error

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.use(cors({
   credentials:true
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
